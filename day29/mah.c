3fe690145b251931a0f7eeb537b0c0dcc61ce871
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-02-23 19:38:45 +0530

    gf

--- day29/mah.c
+++ day29/mah.c
@@ -1,29 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 void main(){
-    float percentages;
-
-    printf("\n enter your percentage");
-    scanf("%f",&percentages);
-
-    if(percentages>=0 && percentages<33){
-        printf("\n failed");
+    for (int i=1;i<=5;i++){
+        for(int j=1;j<=3;j++){
+            printf(" pudi-sbaji");
+        }
+    
+            printf("\n");
+        }
     }
-     else if(percentages>=33 && percentages<50){
-        printf("\n grade D");
-    }
-    else if(percentages>=50 && percentages<60){
-        printf("\n GARDE C");
-    }
-    else if(percentages>=60 && percentages<70){
-        printf("\n garde b");
-    }
-    else if(percentages>=70 && percentages<80){
-        printf("\n garde a");
-    }
-    else if(percentages>=80 && percentages<100){
-        printf("\n garde a+");
-    }
-    else{
-        printf("\n invalid input");
-    }
-}
