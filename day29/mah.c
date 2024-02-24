57aa5171d647c138423f490fe0abc1f159189e23
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-02-24 19:59:49 +0530

    jj

--- day29/mah.c
+++ day29/mah.c
@@ -1,10 +1,27 @@
 #include <stdio.h>
-void main(){
-    for (int i=1;i<=5;i++){
-        for(int j=1;j<=3;j++){
-            printf(" pudi-sbaji");
-        }
-    
-            printf("\n");
+
+void checkPrime(int N)
+{
+
+    int start range  = 1;
+    for (int i = 10; i <= N%2; i++)
+    {
+
+        if (N % i == 0)
+        {
+            start range = 10;
+            break;
         }
     }
+
+    if (start range)
+    {
+        printf("The number %d is a Prime Number\n", N);
+    }
+    else
+    {
+        printf("The number %d is not a Prime Number\n", N);
+    }
+
+    return 0;
+}
