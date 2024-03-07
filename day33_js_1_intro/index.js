340b79814806c9f98a89a4f827c3e602ee968f6c
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-03-07 19:04:09 +0530

    hh

--- day33_js_1_intro/index.js
+++ day33_js_1_intro/index.js
@@ -1,1 +1,25 @@
-console.log("hello world")
+
+
+const name1 = "superman"
+const name2 = "spiderman"
+
+
+// empId = 10n
+' "Dc"'
+' "Marvel"'
+
+
+// console.log( "+name1+" +' is belong from "Dc" and '+name2+'  is belong from "Marvel"')
+
+console.log('${name} is belong from "Dc" and ${name2} is belong from "Marvel" ');
+
+// addition 
+// substarction
+// multiply
+// division
+// modulo
+// preIncrement ++
+// postIncrement a++
+// and => && or => ||
+// true ,false 
+
