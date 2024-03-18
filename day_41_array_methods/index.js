1137a95d335ce24e9c7b8f703a05159520265f3e
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-03-18 19:42:05 +0530

    hh

---
+++ day_41_array_methods/index.js
@@ -0,0 +1,16 @@
+// const student ={
+//     name:"prashant",
+//     rollno:1233,
+//     gmail:"prashna@gamil.com",
+//     course:"frontend"
+// }
+
+// const phone1 ="iphone"
+// const phone1 ="oppo"
+// const phone1 ="samsung"
+
+const phone =["iphone","oneplus","samsung","oppo","vivo","mi"]
+
+for(let item of phone){
+    console.log(item)
+}
