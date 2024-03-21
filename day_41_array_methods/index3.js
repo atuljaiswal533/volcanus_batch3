ad8c55469f6133467ba79cb4799bcf0fd24a3079
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-03-21 18:42:34 +0530

    jj

--- day_41_array_methods/index3.js
+++ day_41_array_methods/index3.js
@@ -1,28 +1,5 @@
-// for - number of iteration know 
-//while - numbre of iteraarioon unknow
-//do-while- atleast run once
-//for in- key value{object}
-//for of-to iterate the array
-//for each- element,index,array
+const arr =["oneplus","iphone","sony","onelusa","iphonea","sonya"];
+const arr =["oneplus","iphone","sony","onelus",];
 
-// let number = 10
-// let sum =0;
-// while(number !==0){
-//     sum +=number;
-//     number--
-// }
-
-// console.log("sum is =" +sum);
-
-let number =456789
-let result =0
-
-while(num ==0){
-    let reminder = num%10
-    console.log("reminder at this point",reminder)
-    result += reminder
-    console.log("sum  at this point",result)
-    num = Math.floor(num/10)
-    
-}
-console.log("final sum =", result)
+//
+console.log (arr.splice(2))
---
+++ day_41_array_methods/index5.js
@@ -0,0 +1,10 @@
+// let name = ['gfg', 'cse', 'geeks', 'portal'];
+// a = name.indexOf('gfg')
+
+// console.log(a)
+
+const fruit = ['gfg', 'cse', 'geeks', 'portal'];
+const isincluded = fruit.includes('gfg');
+
+console.log(isincluded)
+
