0bb48a3b8e248a245c3356b222d21a67bd9e29d9
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-03-27 19:30:24 +0530

    hh

--- day_41_array_methods/index5.js
+++ day_41_array_methods/index5.js
@@ -3,8 +3,21 @@
 
 // console.log(a)
 
-const fruit = ['gfg', 'cse', 'geeks', 'portal'];
-const isincluded = fruit.includes('gfg');
+// const fruit = ['gfg', 'cse', 'geeks', 'portal'];
+// const isincluded = fruit.includes('gfg');
 
-console.log(isincluded)
+// console.log(isincluded)
 
+
+// const square = function (number) {
+// 	return number * number;
+// };
+// const x = square(4); // x gets the value 16
+// console.log(x);
+
+
+const square = function (number) {
+	return number * number;
+};
+const x = square(4); // x gets the value 16
+console.log(x);
