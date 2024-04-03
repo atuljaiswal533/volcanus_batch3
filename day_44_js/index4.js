b72b4d0feb5eca807ef33b4ec2d53ff000351753
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-04-03 21:04:17 +0530

    jj

---
+++ day_44_js/index4.js
@@ -0,0 +1,20 @@
+// const x = 22;
+// {
+//     const x = 90;
+//     console.log(x);
+
+//     {
+//         const x = 77;
+//         console.log(x);
+//     }
+//     {
+//         const x = 45;
+//         console.log(x);
+//     }
+// }
+// console.log(x);
+"usestrict"
+a= 10;
+ console.log(a);
+ var a;
+
