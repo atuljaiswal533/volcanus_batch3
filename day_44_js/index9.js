f45e62055c3d74012ad05566a1d247b5c6f9f491
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-04-11 19:14:07 +0530

    hgg

--- day_44_js/index9.js
+++ day_44_js/index9.js
@@ -1,4 +1,21 @@
+//volume of cylender
+
+
 const r = 5;
+
 const h = 10;
 const vol = (1 / 3) * Math.PI * Math.pow(r, 2) * h;
 console.log(vol);
+
+// compound inetrest ,formala 
+
+const principal = 2000;
+const time = 5;
+const rate = .08;
+const n = 12;
+const compoundInterest = (p, t, r, n) => {
+   const amount = p * (Math.pow((1 + (r / n)), (n * t)));
+   const interest = amount - p;
+   return interest;
+};
+console.log(compoundInterest(principal, time, rate, n));
