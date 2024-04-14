f5840cb12a50a00fd0f05781329613300ef8ae17
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-04-14 10:10:50 +0530

    jj

---
+++ day_44_js/index15.js
@@ -0,0 +1,10 @@
+// Here a date has been assigned
+// while creating a Date object
+let DateObj = new Date('October 35, 1996 05:35:32');
+
+// minutes from above DateObj is
+// being extracted using getMinutes()
+let minutes = DateObj.getMinutes();
+
+// Printing minutes
+console.log(minutes);
