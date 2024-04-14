4d23f5334d9498aa9ff73927c9b052023c87286a
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-04-14 19:20:49 +0530

    kk

--- day_44_js/index15.js
+++ day_44_js/index15.js
@@ -1,10 +1,22 @@
+// // Here a date has been assigned
+// // while creating a Date object
+// let DateObj = new Date('October 35, 1996 05:35:32');
+
+// // minutes from above DateObj is
+// // being extracted using getMinutes()
+// let minutes = DateObj.getMinutes();
+
+// // Printing minutes
+// console.log(minutes);
+
+
 // Here a date has been assigned
-// while creating a Date object
-let DateObj = new Date('October 35, 1996 05:35:32');
+// while creating Date object
+let DateObj = new Date('October 15, 1996 05:35:32:77');
 
-// minutes from above DateObj is
-// being extracted using getMinutes()
-let minutes = DateObj.getMinutes();
+// millisecond from above date object is
+// being extracted using getMilliseconds().
+let millisec = DateObj.getMilliseconds();
 
-// Printing minutes
-console.log(minutes);
+// Printing millisecond
+console.log(millisec);
