47a12ab25559991ffdb5bbb03c2de3878893c350
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-06-03 15:25:35 +0530

    hh

--- day_41_array_methods/index2.js
+++ day_41_array_methods/index2.js
@@ -5,3 +5,10 @@ function myFunction(p1, p2) {
 
 let result = myFunction(4, 3);
 document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
