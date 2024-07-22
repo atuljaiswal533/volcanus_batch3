1f5a720e75403a508963f36e2ed25342035ea5c4
atuljaiswal533 <jaiswalatul533@gmail.com>
2024-07-22 18:59:03 +0530

    Initial commit

--- day_41_array_methods/index7.js
+++ day_41_array_methods/index7.js
@@ -239,3 +239,243 @@ function myFunction(p1, p2) {
   
   document.getElementById("demo").innerHTML = result;
   
+  
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+
+
+function myFunction(p1, p2) {
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  document.getElementById("demo").innerHTML = result;
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  document.getElementById("demo").innerHTML = result;
+  
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+
+
+function myFunction(p1, p2) {
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  document.getElementById("demo").innerHTML = result;
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+  
+  let result = myFunction(4, 3);
+  document.getElementById("demo").innerHTML = result;
+  
+  document.getElementById("demo").innerHTML = result;
+  
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+function myFunction(p1, p2) {
+  return p1 * p2;
+}
+
+let result = myFunction(4, 3);
+document.getElementById("demo").innerHTML = result;
+
+document.getElementById("demo").innerHTML = result;
